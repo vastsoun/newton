d65babde8a97a140abb7dcddb0e3de3ba06f9dfb
Vassilios Tsounis <vassilios.tsounis@disney.com>
2025-09-09 11:30:05 +0200

    WIP: Add numpy/scipy reference solvers

--- newton/_src/solvers/kamino/utils/linalg/linsys.py
+++ newton/_src/solvers/kamino/utils/linalg/linsys.py
@@ -37,7 +37,7 @@ class FixedPointSolution:
 ###
 
 
-def _check_compatibility(A: np.ndarray, b: np.ndarray) -> None:
+def _check_system_compatibility(A: np.ndarray, b: np.ndarray) -> None:
     if A.ndim != 2 or A.shape[0] != A.shape[1]:
         raise ValueError(f"Matrix A must be square (n x n) but has shape {A.shape}.")
     if b.ndim != 1 or b.shape[0] != A.shape[0]:
@@ -46,13 +46,13 @@ def _check_compatibility(A: np.ndarray, b: np.ndarray) -> None:
         raise ValueError(f"Vector b ({b.dtype}) must have the same data type as matrix A ({A.dtype}).")
 
 
-def _check_initial_guess(b: np.ndarray, x_0: np.ndarray | None) -> np.ndarray:
+def _check_initial_guess(A: np.ndarray, x_0: np.ndarray | None) -> np.ndarray:
     if x_0 is None:
-        return np.zeros(b.shape[0], dtype=b.dtype)
-    if x_0.ndim != 1 or x_0.shape[0] != b.shape[0]:
-        raise ValueError(f"Initial guess x_0 ({x_0.shape}) must have compatible dimensions with b ({b.shape}).")
-    if x_0.dtype != b.dtype:
-        raise ValueError(f"Initial guess x_0 ({x_0.dtype}) must have the same data type as vector b ({b.dtype}).")
+        return np.zeros(A.shape[1], dtype=A.dtype)
+    if x_0.ndim != 1 or x_0.shape[0] != A.shape[1]:
+        raise ValueError(f"Initial guess x_0 ({x_0.shape}) must have compatible dimensions with A ({A.shape}).")
+    if x_0.dtype != A.dtype:
+        raise ValueError(f"Initial guess x_0 ({x_0.dtype}) must have the same data type as matrix A ({A.dtype}).")
     return x_0
 
 
@@ -67,29 +67,10 @@ def jacobi(
     x_0: np.ndarray | None,
     tolerance: float = 1e-12,
     max_iterations: int = 1000,
-    verbose: bool = False,
 ) -> FixedPointSolution:
-    # Check if inputs are compatible
-    if A.ndim != 2 or A.shape[0] != A.shape[1]:
-        raise ValueError(f"Matrix A must be square (n x n) but has shape {A.shape}.")
-    if b.ndim != 1 or b.shape[0] != A.shape[0]:
-        raise ValueError(f"Vector b ({b.shape}) must have compatible dimensions with A ({A.shape}).")
-    if b.dtype != A.dtype:
-        raise ValueError(f"Vector b ({b.dtype}) must have the same data type as matrix A ({A.dtype}).")
+    _check_system_compatibility(A, b)
+    x_0 = _check_initial_guess(A, x_0)
 
-    # Set initial guess to zeros if not provided
-    if x_0 is None:
-        x_0 = np.zeros_like(b)
-
-    # TODO
-    if verbose:
-        D = np.diag(np.diag(A))
-        L = np.tril(A, -1)
-        U = np.triu(A, 1)
-        S = -np.linalg.inv(D) @ (L + U)
-        print("[Jacobi]: Iteration Matrix: S:\n", S)
-
-    # Initialize internals
     n = A.shape[0]
     x_p = x_0.copy()
     x_n = x_0.copy()
@@ -117,18 +98,19 @@ def jacobi(
     return solution
 
 
-def gauss_seidel(A, b, x0, tolerance, max_iterations, verbose=False):
-    solution = FixedPointSolution()
+def gauss_seidel(
+    A: np.ndarray,
+    b: np.ndarray,
+    x_0: np.ndarray | None,
+    tolerance: float = 1e-12,
+    max_iterations: int = 1000,
+) -> FixedPointSolution:
+    _check_system_compatibility(A, b)
+    x_0 = _check_initial_guess(A, x_0)
 
-    if verbose:
-        D = np.diag(np.diag(A))
-        L = np.tril(A, -1)
-        U = np.triu(A, 1)
-        S = -np.linalg.inv(D + L) @ U
-        print("[GaussSeidel]: Iteration Matrix: S:\n", S)
-
-    x_n = x0.copy()
     n = A.shape[0]
+    x_n = x_0.copy()
+    solution = FixedPointSolution()
 
     for i in range(max_iterations):
         solution.iterations = i + 1
@@ -140,31 +122,35 @@ def gauss_seidel(A, b, x0, tolerance, max_iterations, verbose=False):
                     sum_ += A[j, k] * x_n[k]
             x_n[j] = (b[j] - sum_) / A[j, j]
 
-        solution.error = np.max(np.abs(x_n - x0))
+        solution.error = np.max(np.abs(x_n - x_0))
 
         if solution.error < tolerance:
             solution.converged = True
             break
 
-        x0[:] = x_n
+        x_0[:] = x_n
 
     solution.x = x_n
     return solution
 
 
-def successive_over_relaxation(A, b, x0, omega, tolerance, max_iterations, verbose=False):
-    solution = FixedPointSolution()
+def successive_over_relaxation(
+    A: np.ndarray,
+    b: np.ndarray,
+    x_0: np.ndarray | None,
+    omega: float = 1.0,
+    tolerance: float = 1e-12,
+    max_iterations: int = 1000,
+) -> FixedPointSolution:
+    _check_system_compatibility(A, b)
+    x_0 = _check_initial_guess(A, x_0)
+    if not (0.0 < omega < 2.0):
+        raise ValueError(f"Relaxation factor omega must be in the range (0, 2) but is {omega}.")
 
-    if verbose:
-        D = np.diag(np.diag(A))
-        L = np.tril(A, -1)
-        U = np.triu(A, 1)
-        S = -np.linalg.inv(D + omega * L) @ (omega * U + (omega - 1.0) * D)
-        print("[SuccessiveOverRelaxation]: Iteration Matrix: S:\n", S)
-
-    x_p = x0.copy()
-    x_n = x0.copy()
     n = A.shape[0]
+    x_p = x_0.copy()
+    x_n = x_0.copy()
+    solution = FixedPointSolution()
 
     for i in range(max_iterations):
         solution.iterations = i + 1
@@ -188,16 +174,24 @@ def successive_over_relaxation(A, b, x0, omega, tolerance, max_iterations, verbo
     return solution
 
 
-def conjugate_gradient(A, b, x0, epsilon, tolerance, max_iterations):
-    solution = FixedPointSolution()
+def conjugate_gradient(
+    A: np.ndarray,
+    b: np.ndarray,
+    x_0: np.ndarray | None,
+    epsilon: float = 1e-12,
+    tolerance: float = 1e-12,
+    max_iterations: int = 1000,
+) -> FixedPointSolution:
+    _check_system_compatibility(A, b)
+    x_0 = _check_initial_guess(A, x_0)
 
-    x = x0.copy()
-    r = b - A @ x
-    g = r.copy()
     n = A.shape[0]
-
+    r = b - A @ x_0
+    x = x_0.copy()
+    g = r.copy()
     r_old = np.zeros(n)
     Ag = np.zeros(n)
+    solution = FixedPointSolution()
 
     for i in range(max_iterations):
         solution.iterations = i + 1
@@ -221,19 +215,27 @@ def conjugate_gradient(A, b, x0, epsilon, tolerance, max_iterations):
     return solution
 
 
-def minimum_residual(A, b, x0, epsilon, tolerance, max_iterations):
-    solution = FixedPointSolution()
+def minimum_residual(
+    A: np.ndarray,
+    b: np.ndarray,
+    x_0: np.ndarray | None,
+    epsilon: float = 1e-12,
+    tolerance: float = 1e-12,
+    max_iterations: int = 1000,
+) -> FixedPointSolution:
+    _check_system_compatibility(A, b)
+    x_0 = _check_initial_guess(A, x_0)
 
     n = A.shape[0]
-    x = x0.copy()
-    r = b - A @ x0
-
+    r = b - A @ x_0
+    x = x_0.copy()
     p0 = r.copy()
     s0 = A @ p0
     p1 = p0.copy()
     s1 = s0.copy()
     p2 = np.zeros(n)
     s2 = np.zeros(n)
+    solution = FixedPointSolution()
 
     for i in range(max_iterations):
         solution.iterations = i + 1
@@ -278,8 +280,8 @@ def minimum_residual(A, b, x0, epsilon, tolerance, max_iterations):
 # ---------------------------
 if __name__ == "__main__":
     # ----------------------------
-    # dtype = np.float64
-    dtype = np.float32
+    dtype = np.float64
+    # dtype = np.float32
     print("----------------------------")
     print(f"dtype: {dtype}")
 
@@ -297,7 +299,7 @@ if __name__ == "__main__":
     print(f"\nx_np {x_np.shape}[{x_np.dtype}]:\n{x_np}\n")
 
     # ---------------------------- Jacobi
-    jac = jacobi(A=A, b=b, x_0=None, tolerance=1e-15, max_iterations=1000, verbose=True)
+    jac = jacobi(A=A, b=b, x_0=None, tolerance=1e-15, max_iterations=1000)
     r_jac = A @ jac.x - b
     print("----------------------------")
     print(f"Jacobi:  converged: {jac.converged}")
@@ -306,14 +308,74 @@ if __name__ == "__main__":
     print("----------------------------")
     print(f"\nx_jac {jac.x.shape}[{jac.x.dtype}]:\n{jac.x}\n")
 
+    # ---------------------------- Gauss-Seidel
+    gs = gauss_seidel(A=A, b=b, x_0=None, tolerance=1e-15, max_iterations=1000)
+    r_gs = A @ gs.x - b
+    print("----------------------------")
+    print(f"Gauss-Seidel:  converged: {gs.converged}")
+    print(f"Gauss-Seidel: iterations: {gs.iterations}")
+    print(f"Gauss-Seidel:      error: {gs.error}")
+    print("----------------------------")
+    print(f"\nx_gs {gs.x.shape}[{gs.x.dtype}]:\n{gs.x}\n")
+
+    # ---------------------------- Successive Over-Relaxation
+    sor = successive_over_relaxation(A=A, b=b, x_0=None, omega=1.25, tolerance=1e-15, max_iterations=1000)
+    r_sor = A @ sor.x - b
+    print("----------------------------")
+    print(f"Successive Over-Relaxation:  converged: {sor.converged}")
+    print(f"Successive Over-Relaxation: iterations: {sor.iterations}")
+    print(f"Successive Over-Relaxation:      error: {sor.error}")
+    print("----------------------------")
+    print(f"\nx_sor {sor.x.shape}[{sor.x.dtype}]:\n{sor.x}\n")
+
+    # ---------------------------- Conjugate Gradient
+    cg = conjugate_gradient(A=A, b=b, x_0=None, epsilon=1e-15, tolerance=1e-15, max_iterations=1000)
+    r_cg = A @ cg.x - b
+    print("----------------------------")
+    print(f"Conjugate Gradient:  converged: {cg.converged}")
+    print(f"Conjugate Gradient: iterations: {cg.iterations}")
+    print(f"Conjugate Gradient:      error: {cg.error}")
+    print("----------------------------")
+    print(f"\nx_cg {cg.x.shape}[{cg.x.dtype}]:\n{cg.x}\n")
+
+    # ---------------------------- Minimum Residual
+    minres = minimum_residual(A=A, b=b, x_0=None, epsilon=1e-15, tolerance=1e-15, max_iterations=1000)
+    r_minres = A @ minres.x - b
+    print("----------------------------")
+    print(f"Minimum Residual:  converged: {minres.converged}")
+    print(f"Minimum Residual: iterations: {minres.iterations}")
+    print(f"Minimum Residual:      error: {minres.error}")
+    print("----------------------------")
+    print(f"\nx_minres {minres.x.shape}[{minres.x.dtype}]:\n{minres.x}\n")
+
     # ----------------------------
     r_np_l2 = np.linalg.norm(r_np, ord=2)
     r_jac_l2 = np.linalg.norm(r_jac, ord=2)
+    r_gs_l2 = np.linalg.norm(r_gs, ord=2)
+    r_sor_l2 = np.linalg.norm(r_sor, ord=2)
+    r_cg_l2 = np.linalg.norm(r_cg, ord=2)
+    r_minres_l2 = np.linalg.norm(r_minres, ord=2)
+
+    # ----------------------------
     r_np_infnorm = np.linalg.norm(r_np, ord=np.inf)
     r_jac_infnorm = np.linalg.norm(r_jac, ord=np.inf)
+    r_gs_infnorm = np.linalg.norm(r_gs, ord=np.inf)
+    r_sor_infnorm = np.linalg.norm(r_sor, ord=np.inf)
+    r_cg_infnorm = np.linalg.norm(r_cg, ord=np.inf)
+    r_minres_infnorm = np.linalg.norm(r_minres, ord=np.inf)
+
+    # ----------------------------
     print("----------------------------")
     print(f"r_np_l2: {r_np_l2}")
     print(f"r_jac_l2: {r_jac_l2}")
+    print(f"r_gs_l2: {r_gs_l2}")
+    print(f"r_sor_l2: {r_sor_l2}")
+    print(f"r_cg_l2: {r_cg_l2}")
+    print(f"r_minres_l2: {r_minres_l2}")
     print("----------------------------")
     print(f"r_np_infnorm: {r_np_infnorm}")
     print(f"r_jac_infnorm: {r_jac_infnorm}")
+    print(f"r_gs_infnorm: {r_gs_infnorm}")
+    print(f"r_sor_infnorm: {r_sor_infnorm}")
+    print(f"r_cg_infnorm: {r_cg_infnorm}")
+    print(f"r_minres_infnorm: {r_minres_infnorm}")

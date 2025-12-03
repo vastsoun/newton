d05d8c6b20f3482d9417f6d6fb50a39a7d0e0238
Eric Heiden <eheiden@nvidia.com>
2025-12-02 16:00:33 -0800

    Fix joint transform for world-attached joints to use MJCF xform argument (#1171)
    Signed-off-by: Eric Heiden <eric-heiden@outlook.com>

--- newton/_src/utils/import_mjcf.py
+++ newton/_src/utils/import_mjcf.py
@@ -743,6 +743,12 @@ def parse_mjcf(
                 )
             else:
                 # TODO parse ref, springref values from joint_attrib
+                # When parent is world (-1), use world_xform to respect the xform argument
+                if parent == -1:
+                    parent_xform_for_joint = world_xform * wp.transform(joint_pos, wp.quat_identity())
+                else:
+                    parent_xform_for_joint = wp.transform(body_pos_for_joints + joint_pos, body_ori_for_joints)
+
                 joint_indices.append(
                     builder.add_joint(
                         joint_type,
@@ -751,7 +757,7 @@ def parse_mjcf(
                         linear_axes=linear_axes,
                         angular_axes=angular_axes,
                         key="_".join(joint_name),
-                        parent_xform=wp.transform(body_pos_for_joints + joint_pos, body_ori_for_joints),
+                        parent_xform=parent_xform_for_joint,
                         child_xform=wp.transform(joint_pos, wp.quat_identity()),
                         custom_attributes=joint_custom_attributes | dof_custom_attributes,
                     )

0d8faf48b57051d56627f49aa80e3a1291fe18ed
agon.serifi <agon.serifi@disney.com>
2025-12-09 16:30:20 +0100

    Always show contacts in box on plane and boxes fourbar

--- newton/_src/solvers/kamino/examples/sim/example_sim_basics_box_on_plane.py
+++ newton/_src/solvers/kamino/examples/sim/example_sim_basics_box_on_plane.py
@@ -125,7 +125,6 @@ class Example:
         ground: bool = True,
         logging: bool = False,
         headless: bool = False,
-        show_contacts: bool = False,
         record_video: bool = False,
         async_save: bool = False,
     ):
@@ -203,7 +202,7 @@ class Example:
             self.viewer = ViewerKamino(
                 builder=self.builder,
                 simulator=self.sim,
-                show_contacts=show_contacts,
+                show_contacts=True,
                 record_video=record_video,
                 video_folder=video_folder,
                 async_save=async_save,
@@ -321,7 +320,6 @@ if __name__ == "__main__":
     parser.add_argument("--clear-cache", action="store_true", default=False, help="Clear warp cache")
     parser.add_argument("--logging", action="store_true", default=True, help="Enable logging of simulation data")
     parser.add_argument("--show-plots", action="store_true", default=False, help="Show plots of logging data")
-    parser.add_argument("--show-contacts", action="store_true", default=False, help="Enable contact visualization")
     parser.add_argument("--test", action="store_true", default=False, help="Run tests")
     parser.add_argument(
         "--record",
@@ -368,7 +366,6 @@ if __name__ == "__main__":
         gravity=args.gravity,
         ground=args.ground,
         headless=args.headless,
-        show_contacts=args.show_contacts,
         logging=args.logging,
         record_video=args.record is not None and not args.headless,
         async_save=args.record == "async",

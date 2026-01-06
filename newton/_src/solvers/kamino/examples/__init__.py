6cb7eb60350e3aa8467cd9ca597a20c4b4bf95e1
Guirec-Maloisel <25688871+Guirec-Maloisel@users.noreply.github.com>
2026-01-06 09:37:53 +0100

    Fix division by zero in progress bar

--- newton/_src/solvers/kamino/examples/__init__.py
+++ newton/_src/solvers/kamino/examples/__init__.py
@@ -90,7 +90,7 @@ def print_progress_bar(iteration, total, start_time, length=40, prefix="", suffi
     bar = "█" * filled_length + "-" * (length - filled_length)
 
     # Estimated Time of Arrival
-    if iteration > 0:
+    if iteration > 0 and elapsed > 0:
         eta = elapsed / iteration * (total - iteration)
         eta_str = time.strftime("%H:%M:%S", time.gmtime(eta))
         fps = iteration / elapsed

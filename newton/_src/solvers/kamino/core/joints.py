Fix computation of joint data (now works and is unit-tested)
Details:
- Rename q_j_ref to q_j_p to avoid name collisions w/ PD references
- Fix operations in generated kernel (some offsets were wrong)
- Add unit tests for computing state-dependent joint dynamic parameters
- Change JointDescriptor is_implicit property to is_dynamic and is_implicit_pd to be clearer and more accurate
- add checks to JointDescriptor to forbid creation of PD-controlled FREE and dynamic FIXED joints
-

newton/_src/solvers/kamino/core/joints.py
@@ -859,7 +859,7 @@ class JointDescriptor(Descriptor):
         """
         Returns the number of dynamic constraints introduced by this joint.
         """
-        return self.dof_type.num_dofs if self.is_implicit else 0
+        return self.dof_type.num_dofs if self.is_dynamic else 0
 
     @property
     def num_kinematic_cts(self) -> int:
@@ -896,14 +896,19 @@ class JointDescriptor(Descriptor):
         """
         return self.act_type > JointActuationType.PASSIVE
 
-    # TODO: @ruben, should we rename this to `is_dynamic`?
-    # TODO: @ruben, should we also add a `is_implicit_pd` property method?
     @property
-    def is_implicit(self) -> bool:
+    def is_dynamic(self) -> bool:
         """
         Returns whether the joint's dynamics is simulated implicitly.
         """
         return np.any(self.a_j) or np.any(self.b_j)
+
+    @property
+    def is_implicit_pd(self) -> bool:
+        """
+        Returns whether the joint's dynamics is simulated using implicit PD control.
+        """
+        return np.any(self.k_p_j) or np.any(self.k_d_j)
 
     def has_base_body(self, bid: int) -> bool:
         """
@@ -947,6 +952,20 @@ class JointDescriptor(Descriptor):
         if self.act_type == JointActuationType.FORCE and self.dof_type == JointDoFType.FIXED:
             raise ValueError(
                 f"Invalid joint configuration: FIXED joints cannot be actuated (name={self.name}, uid={self.uid})."
+            )
+
+        # Check if DoF type + dynamic/implicit PD settings are compatible
+        if self.is_implicit_pd and self.dof_type == JointDoFType.FREE:
+            raise ValueError(
+                f"Invalid joint configuration: FREE joints cannot have implicit PD gains (name={self.name}, uid={self.uid})."
+            )
+        if self.is_dynamic and self.dof_type == JointDoFType.FIXED:
+            raise ValueError(
+                f"Invalid joint configuration: FIXED joints cannot be dynamic (name={self.name}, uid={self.uid})."
+            )
+        if self.is_implicit_pd and self.dof_type == JointDoFType.FIXED:
+            raise ValueError(
+                f"Invalid joint configuration: FIXED joints cannot have implicit PD gains (name={self.name}, uid={self.uid})."
             )
 
         # Set default values for joint limits if not provided
@@ -1577,13 +1596,31 @@ class JointsData:
 
     v_b_dyn_j: wp.array | None = None
     """
-    The velocity bias of the joint dynamic constraints (as flat array),
+    The velocity bias of the joint dynamic constraints (as flat array).
 
-    ``h_j := dt * ( k_p_j * ( q_j_ref - q_j ) + k_d_j * dq_j_ref ) ``,\n
-    ``v_b_dyn_j := inv_m_j * ( a_j * dq_j + dt * h_j ) ``,\n
+    Each joint has local actuation and PD control dynamics:\n
+    ``m_j * dq_j^{+} = a_j * dq_j^{-} + dt * h_j``
+
+    and is contributes to the dynamice of the system through the constraint equation:\n
+    ``dq_j^{+} = J_a_j * u^{+}``
+
+    where ``dq_j^{-}`` and ``dq_j^{+}`` are the pre- and post-event joint-space
+    velocities, and ``u^{+}`` are the post-event generalized velocities of the
+    system computed implicitly as a result of solving the forward dynamics problem
+    with the joint dynamic constraints. `J_a_j` is the block of the actuation Jacobian
+    matrix corresponding to the rows of DoFs of joint `j`.
+
+    This results in the following dynamic constraint equation for each joint `j`:\n
+    ``dq_j^{+} + m_j^{-1} * lambda_q_j = m_j^{-1} * (a_j * dq_j^{-} + dt * h_j)``,\n
+    ``dq_j^{+} + m_j^{-1} * lambda_q_j = v_b_dyn_j``,\n
+    ``J_a_j * u^{+} + m_j^{-1} * lambda_q_j = v_b_dyn_j``
+
+    and thus the velocity bias term of the joint-space dynamics of each joint `j` is computed as:\n
+    ``h_j := dt * ( k_p_j * ( q_j_ref - q_j^{-} ) + k_d_j * dq_j_ref ) ``,\n
+    ``v_b_dyn_j := inv_m_j * ( a_j * dq_j^{-} + dt * h_j ) ``,\n
     where dt is the simulation time step.
 
-    Shape of ``(sum(e_j),)`` and type :class:`float`,\n
+    Shape of ``(sum(e_j),)`` and type :class:`float`,
     where ``e_j`` is the number of dynamic constraints of joint ``j``.
     """
 

newton/_src/solvers/kamino/tests/test_kinematics_joints.py
@@ -152,7 +152,8 @@ class TestKinematicsJoints(unittest.TestCase):
         if not test_context.setup_done:
             setup_tests(clear_cache=False)
         self.default_device = wp.get_device(test_context.device)
-        self.verbose = test_context.verbose  # Set to True to enable verbose output
+        # self.verbose = test_context.verbose  # Set to True to enable verbose output
+        self.verbose = True  # Set to True to enable verbose output
 
         # Set debug-level logging to print verbose test output to console
         if self.verbose:
@@ -180,7 +181,7 @@ class TestKinematicsJoints(unittest.TestCase):
         msg.info("data.bodies.u_i: %s", data.bodies.u_i)
 
         # Update the state of the joints
-        compute_joints_data(model=model, q_j_ref=wp.zeros_like(data.joints.q_j), data=data)
+        compute_joints_data(model=model, data=data, q_j_p=wp.zeros_like(data.joints.q_j))
         msg.info("data.joints.p_j: %s", data.joints.p_j)
 
         # Extract joint data
@@ -223,7 +224,7 @@ class TestKinematicsJoints(unittest.TestCase):
         msg.info("data.bodies.u_i:\n%s", data.bodies.u_i)
 
         # Update the state of the joints
-        compute_joints_data(model=model, q_j_ref=wp.zeros_like(data.joints.q_j), data=data)
+        compute_joints_data(model=model, data=data, q_j_p=wp.zeros_like(data.joints.q_j))
         msg.info("data.joints.p_j: %s", data.joints.p_j)
 
         # Extract joint data
@@ -257,6 +258,200 @@ class TestKinematicsJoints(unittest.TestCase):
         np.testing.assert_almost_equal(dr_j_np, dr_j_expected)
         np.testing.assert_almost_equal(q_j_np, q_j_expected)
         np.testing.assert_almost_equal(dq_j_np, dq_j_expected)
+
+    def test_03_single_dynamic_revolute_joint(self):
+        # Construct the model description using the ModelBuilder
+        builder = build_unary_revolute_joint_test(dynamic=True, implicit_pd=True)
+
+        # Create the model and state
+        model = builder.finalize(device=self.default_device)
+        data = model.data(device=self.default_device)
+        model.time.set_uniform_timestep(0.01)
+
+        # Optionally print model parameters for debugging
+        msg.info("model.time.dt: %s", model.time.dt)
+        msg.info("model.joints.a_j: %s", model.joints.a_j)
+        msg.info("model.joints.b_j: %s", model.joints.b_j)
+        msg.info("model.joints.k_p_j: %s", model.joints.k_p_j)
+        msg.info("model.joints.k_d_j: %s\n", model.joints.k_d_j)
+        msg.info("model.joints.num_cts: %s", model.joints.num_cts)
+        msg.info("model.joints.num_dynamic_cts: %s", model.joints.num_dynamic_cts)
+        msg.info("model.joints.num_kinematic_cts: %s", model.joints.num_kinematic_cts)
+        msg.info("model.joints.dynamic_cts_offset: %s", model.joints.dynamic_cts_offset)
+        msg.info("model.joints.kinematic_cts_offset: %s\n", model.joints.kinematic_cts_offset)
+        msg.info("model.info.num_joint_dynamic_cts: %s", model.info.num_joint_dynamic_cts)
+        msg.info("model.info.joint_dynamic_cts_offset: %s\n", model.info.joint_dynamic_cts_offset)
+
+        # Set the state of the Follower body to a known state
+        set_joint_follower_body_state(model, data)
+        msg.info("data.bodies.q_i: %s", data.bodies.q_i)
+        msg.info("data.bodies.u_i: %s\n", data.bodies.u_i)
+
+        # Update the state of the joints
+        compute_joints_data(model=model, data=data, q_j_p=wp.zeros_like(data.joints.q_j))
+        msg.info("data.joints.p_j: %s\n", data.joints.p_j)
+
+        # Extract joint data
+        r_j_np = data.joints.r_j.numpy().copy()
+        dr_j_np = data.joints.dr_j.numpy().copy()
+        q_j_np = data.joints.q_j.numpy().copy()
+        dq_j_np = data.joints.dq_j.numpy().copy()
+        m_j_np = data.joints.m_j.numpy().copy()
+        inv_m_j_np = data.joints.inv_m_j.numpy().copy()
+        v_b_dyn_j_np = data.joints.v_b_dyn_j.numpy().copy()
+        q_j_ref_np = data.joints.q_j_ref.numpy().copy()
+        dq_j_ref_np = data.joints.dq_j_ref.numpy().copy()
+        msg.info("[measured]:  r_j: %s", r_j_np)
+        msg.info("[measured]: dr_j: %s", dr_j_np)
+        msg.info("[measured]:  q_j: %s", q_j_np)
+        msg.info("[measured]: dq_j: %s\n", dq_j_np)
+        msg.info("[measured]: m_j: %s", m_j_np)
+        msg.info("[measured]: inv_m_j: %s", inv_m_j_np)
+        msg.info("[measured]: v_b_dyn_j: %s\n", v_b_dyn_j_np)
+        msg.info("[measured]: q_j_ref: %s", q_j_ref_np)
+        msg.info("[measured]: dq_j_ref: %s\n", dq_j_ref_np)
+
+        # Construct expected joint data
+        dt = model.time.dt.numpy().copy()[0]
+        a_j_np = model.joints.a_j.numpy().copy()
+        b_j_np = model.joints.b_j.numpy().copy()
+        k_p_j_np = model.joints.k_p_j.numpy().copy()
+        k_d_j_np = model.joints.k_d_j.numpy().copy()
+        r_j_expected = np.array([J_DR_J[0], J_DR_J[1], J_DR_J[2], ROT_RES[1], ROT_RES[2]], dtype=np.float32)
+        dr_j_expected = np.array([J_DV_J[0], J_DV_J[1], J_DV_J[2], J_DOMEGA_J[1], J_DOMEGA_J[2]], dtype=np.float32)
+        q_j_expected = np.array([Q_X_J], dtype=np.float32)
+        dq_j_expected = np.array([J_DOMEGA_J[0]], dtype=np.float32)
+        m_j_exp_val = a_j_np[0] + dt * (b_j_np[0] + k_d_j_np[0]) + dt * dt * k_p_j_np[0]
+        inv_m_j_exp_val = 1.0 / m_j_exp_val
+        h_j_exp_val = dt * (k_p_j_np[0] * (q_j_ref_np[0] - q_j_expected[0]) + k_d_j_np[0] * dq_j_ref_np[0])
+        v_b_dyn_j_exp_val = inv_m_j_exp_val * (a_j_np[0] * dq_j_np[0] + dt * h_j_exp_val)
+        m_j_expected = np.array([m_j_exp_val], dtype=np.float32)
+        h_j_expected = np.array([h_j_exp_val], dtype=np.float32)
+        inv_m_j_expected = np.array([inv_m_j_exp_val], dtype=np.float32)
+        v_b_dyn_j_expected = np.array([v_b_dyn_j_exp_val], dtype=np.float32)
+        msg.info("[expected]:  r_j: %s", r_j_expected)
+        msg.info("[expected]: dr_j: %s", dr_j_expected)
+        msg.info("[expected]:  q_j: %s", q_j_expected)
+        msg.info("[expected]: dq_j: %s\n", dq_j_expected)
+        msg.info("[expected]: m_j: %s", m_j_expected)
+        msg.info("[expected]: h_j: %s", h_j_expected)
+        msg.info("[expected]: inv_m_j: %s", inv_m_j_expected)
+        msg.info("[expected]: v_b_dyn_j: %s\n", v_b_dyn_j_expected)
+
+        # Check the joint data values
+        np.testing.assert_almost_equal(r_j_np, r_j_expected)
+        np.testing.assert_almost_equal(dr_j_np, dr_j_expected)
+        np.testing.assert_almost_equal(q_j_np, q_j_expected)
+        np.testing.assert_almost_equal(dq_j_np, dq_j_expected)
+        np.testing.assert_almost_equal(m_j_np, m_j_expected)
+        np.testing.assert_almost_equal(inv_m_j_np, inv_m_j_expected)
+        np.testing.assert_almost_equal(v_b_dyn_j_np, v_b_dyn_j_expected)
+
+    def test_04_multiple_dynamic_revolute_joints(self):
+        # Construct the model description using the ModelBuilder
+        builder = make_homogeneous_builder(
+            num_worlds=4, build_fn=build_unary_revolute_joint_test, dynamic=True, implicit_pd=True
+        )
+
+        # Create the model and data
+        model = builder.finalize(device=self.default_device)
+        data = model.data(device=self.default_device)
+        model.time.set_uniform_timestep(0.01)
+
+        # Optionally print model parameters for debugging
+        msg.info("model.time.dt: %s", model.time.dt)
+        msg.info("model.joints.a_j: %s", model.joints.a_j)
+        msg.info("model.joints.b_j: %s", model.joints.b_j)
+        msg.info("model.joints.k_p_j: %s", model.joints.k_p_j)
+        msg.info("model.joints.k_d_j: %s\n", model.joints.k_d_j)
+        msg.info("model.joints.num_cts: %s", model.joints.num_cts)
+        msg.info("model.joints.num_dynamic_cts: %s", model.joints.num_dynamic_cts)
+        msg.info("model.joints.num_kinematic_cts: %s", model.joints.num_kinematic_cts)
+        msg.info("model.joints.dynamic_cts_offset: %s", model.joints.dynamic_cts_offset)
+        msg.info("model.joints.kinematic_cts_offset: %s\n", model.joints.kinematic_cts_offset)
+        msg.info("model.info.num_joint_dynamic_cts: %s", model.info.num_joint_dynamic_cts)
+        msg.info("model.info.joint_dynamic_cts_offset: %s\n", model.info.joint_dynamic_cts_offset)
+
+        # Set the state of the Follower body to a known state
+        set_joint_follower_body_state(model, data)
+        msg.info("data.bodies.q_i:\n%s", data.bodies.q_i)
+        msg.info("data.bodies.u_i:\n%s\n", data.bodies.u_i)
+
+        # Update the state of the joints
+        compute_joints_data(model=model, data=data, q_j_p=wp.zeros_like(data.joints.q_j))
+        msg.info("data.joints.p_j:\n%s", data.joints.p_j)
+
+        # Extract joint data
+        r_j_np = data.joints.r_j.numpy().copy()
+        dr_j_np = data.joints.dr_j.numpy().copy()
+        q_j_np = data.joints.q_j.numpy().copy()
+        dq_j_np = data.joints.dq_j.numpy().copy()
+        m_j_np = data.joints.m_j.numpy().copy()
+        inv_m_j_np = data.joints.inv_m_j.numpy().copy()
+        v_b_dyn_j_np = data.joints.v_b_dyn_j.numpy().copy()
+        q_j_ref_np = data.joints.q_j_ref.numpy().copy()
+        dq_j_ref_np = data.joints.dq_j_ref.numpy().copy()
+        msg.info("[measured]:  r_j: %s", r_j_np)
+        msg.info("[measured]: dr_j: %s", dr_j_np)
+        msg.info("[measured]:  q_j: %s", q_j_np)
+        msg.info("[measured]: dq_j: %s\n", dq_j_np)
+        msg.info("[measured]: m_j: %s", m_j_np)
+        msg.info("[measured]: inv_m_j: %s", inv_m_j_np)
+        msg.info("[measured]: v_b_dyn_j: %s\n", v_b_dyn_j_np)
+        msg.info("[measured]: q_j_ref: %s", q_j_ref_np)
+        msg.info("[measured]: dq_j_ref: %s\n", dq_j_ref_np)
+
+        # Construct expected joint data
+        r_j_expected = np.array([J_DR_J[0], J_DR_J[1], J_DR_J[2], ROT_RES[1], ROT_RES[2]], dtype=np.float32)
+        dr_j_expected = np.array([J_DV_J[0], J_DV_J[1], J_DV_J[2], J_DOMEGA_J[1], J_DOMEGA_J[2]], dtype=np.float32)
+        q_j_expected = np.array([Q_X_J], dtype=np.float32)
+        dq_j_expected = np.array([J_DOMEGA_J[0]], dtype=np.float32)
+
+        # Construct expected joint data
+        dt = model.time.dt.numpy().copy()[0]
+        a_j_np = model.joints.a_j.numpy().copy()
+        b_j_np = model.joints.b_j.numpy().copy()
+        k_p_j_np = model.joints.k_p_j.numpy().copy()
+        k_d_j_np = model.joints.k_d_j.numpy().copy()
+        r_j_expected = np.array([J_DR_J[0], J_DR_J[1], J_DR_J[2], ROT_RES[1], ROT_RES[2]], dtype=np.float32)
+        dr_j_expected = np.array([J_DV_J[0], J_DV_J[1], J_DV_J[2], J_DOMEGA_J[1], J_DOMEGA_J[2]], dtype=np.float32)
+        q_j_expected = np.array([Q_X_J], dtype=np.float32)
+        dq_j_expected = np.array([J_DOMEGA_J[0]], dtype=np.float32)
+        m_j_exp_val = a_j_np[0] + dt * (b_j_np[0] + k_d_j_np[0]) + dt * dt * k_p_j_np[0]
+        inv_m_j_exp_val = 1.0 / m_j_exp_val
+        h_j_exp_val = dt * (k_p_j_np[0] * (q_j_ref_np[0] - q_j_expected[0]) + k_d_j_np[0] * dq_j_ref_np[0])
+        v_b_dyn_j_exp_val = inv_m_j_exp_val * (a_j_np[0] * dq_j_np[0] + dt * h_j_exp_val)
+        m_j_expected = np.array([m_j_exp_val], dtype=np.float32)
+        h_j_expected = np.array([h_j_exp_val], dtype=np.float32)
+        inv_m_j_expected = np.array([inv_m_j_exp_val], dtype=np.float32)
+        v_b_dyn_j_expected = np.array([v_b_dyn_j_exp_val], dtype=np.float32)
+
+        # Tile expected values for all joints
+        r_j_expected = np.tile(r_j_expected, builder.num_worlds)
+        dr_j_expected = np.tile(dr_j_expected, builder.num_worlds)
+        q_j_expected = np.tile(q_j_expected, builder.num_worlds)
+        dq_j_expected = np.tile(dq_j_expected, builder.num_worlds)
+        m_j_expected = np.tile(m_j_expected, builder.num_worlds)
+        h_j_expected = np.tile(h_j_expected, builder.num_worlds)
+        inv_m_j_expected = np.tile(inv_m_j_expected, builder.num_worlds)
+        v_b_dyn_j_expected = np.tile(v_b_dyn_j_expected, builder.num_worlds)
+        msg.info("[expected]:  r_j: %s", r_j_expected)
+        msg.info("[expected]: dr_j: %s", dr_j_expected)
+        msg.info("[expected]:  q_j: %s", q_j_expected)
+        msg.info("[expected]: dq_j: %s\n", dq_j_expected)
+        msg.info("[expected]: m_j: %s", m_j_expected)
+        msg.info("[expected]: h_j: %s", h_j_expected)
+        msg.info("[expected]: inv_m_j: %s", inv_m_j_expected)
+        msg.info("[expected]: v_b_dyn_j: %s\n", v_b_dyn_j_expected)
+
+        # Check the joint data values
+        np.testing.assert_almost_equal(r_j_np, r_j_expected)
+        np.testing.assert_almost_equal(dr_j_np, dr_j_expected)
+        np.testing.assert_almost_equal(q_j_np, q_j_expected)
+        np.testing.assert_almost_equal(dq_j_np, dq_j_expected)
+        np.testing.assert_almost_equal(m_j_np, m_j_expected)
+        np.testing.assert_almost_equal(inv_m_j_np, inv_m_j_expected)
+        np.testing.assert_almost_equal(v_b_dyn_j_np, v_b_dyn_j_expected)
 
 
 ###
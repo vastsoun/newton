ce47166112d20ff385e028b1d8a099f5fc5c0f43
Vassilios Tsounis <vassilios.tsounis@disney.com>
2026-02-23 20:30:30 +0100

    Add some module org cleanup

--- newton/_src/solvers/kamino/utils/benchmark/__init__.py
+++ newton/_src/solvers/kamino/utils/benchmark/__init__.py
@@ -13,14 +13,61 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Utilities for solver benchmarking."""
+"""Utilities for solver configuration benchmarking."""
 
-# from .animation import AnimationJointReference, AnimationJointReferenceData
+from .configs import make_benchmark_configs, make_solver_config_default
+from .metrics import (
+    BenchmarkMetrics,
+    SolverMetrics,
+    StatsBinary,
+    StatsFloat,
+    StatsInteger,
+)
+from .problems import (
+    BenchmarkProblemNameToConfigFn,
+    CameraConfig,
+    ControlConfig,
+    ProblemConfig,
+    ProblemSet,
+    make_benchmark_problems,
+)
+from .render import (
+    render_solver_configs_table,
+    render_subcolumn_metrics_table_rich,
+)
+from .runner import (
+    BenchmarkSim,
+    run_single_benchmark,
+    run_single_benchmark_silent,
+    run_single_benchmark_with_progress,
+    run_single_benchmark_with_step_metrics,
+    run_single_benchmark_with_viewer,
+)
 
 ###
 # Module interface
 ###
 
 __all__ = [
-    # "TODO",
+    "BenchmarkMetrics",
+    "BenchmarkProblemNameToConfigFn",
+    "BenchmarkSim",
+    "CameraConfig",
+    "ControlConfig",
+    "ProblemConfig",
+    "ProblemSet",
+    "SolverMetrics",
+    "StatsBinary",
+    "StatsFloat",
+    "StatsInteger",
+    "make_benchmark_configs",
+    "make_benchmark_problems",
+    "make_solver_config_default",
+    "render_solver_configs_table",
+    "render_subcolumn_metrics_table_rich",
+    "run_single_benchmark",
+    "run_single_benchmark_silent",
+    "run_single_benchmark_with_progress",
+    "run_single_benchmark_with_step_metrics",
+    "run_single_benchmark_with_viewer",
 ]

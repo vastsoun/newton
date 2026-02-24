402960aa492c06fa8d3b3ac836b004e5b95a9265
Vassilios Tsounis <vassilios.tsounis@disney.com>
2026-02-24 13:43:12 +0100

    WIP: Refactor common test problem scaffolding

--- newton/_src/solvers/kamino/tests/test_dynamics_wrenches.py
+++ newton/_src/solvers/kamino/tests/test_dynamics_wrenches.py
@@ -22,7 +22,7 @@ import unittest
 import numpy as np
 import warp as wp
 
-from newton._src.solvers.kamino.core.model import Model
+from newton._src.solvers.kamino.core.model import Model, ModelData
 from newton._src.solvers.kamino.core.types import float32
 from newton._src.solvers.kamino.dynamics.wrenches import (
     compute_constraint_body_wrenches_dense,
@@ -30,12 +30,23 @@ from newton._src.solvers.kamino.dynamics.wrenches import (
     compute_joint_dof_body_wrenches_dense,
     compute_joint_dof_body_wrenches_sparse,
 )
+from newton._src.solvers.kamino.geometry.contacts import Contacts
 from newton._src.solvers.kamino.kinematics.jacobians import DenseSystemJacobians, SparseSystemJacobians
+from newton._src.solvers.kamino.kinematics.limits import Limits
 from newton._src.solvers.kamino.tests import setup_tests, test_context
 from newton._src.solvers.kamino.tests.test_kinematics_jacobians import (
     make_test_problem_fourbar,
 )
 from newton._src.solvers.kamino.utils import logger as msg
+
+###
+# Constants
+###
+
+
+test_wrench_rtol = 1e-7
+test_wrench_atol = 1e-8
+
 
 ###
 # Helper Functions
@@ -46,6 +57,80 @@ def make_constraint_multiplier_arrays(model: Model) -> tuple[wp.array, wp.array]
     with wp.ScopedDevice(model.device):
         lambdas = wp.zeros(model.size.sum_of_max_total_cts, dtype=float32)
     return model.info.total_cts_offset, lambdas
+
+
+def compute_and_compare_dense_sparse_jacobian_wrenches(
+    model: Model,
+    data: ModelData,
+    limits: Limits,
+    contacts: Contacts,
+    rtol: float = 1e-7,
+    atol: float = 0.0,
+):
+    # Create the Jacobians container
+    jacobians_dense = DenseSystemJacobians(model=model, limits=limits, contacts=contacts)
+    jacobians_sparse = SparseSystemJacobians(model=model, limits=limits, contacts=contacts)
+    wp.synchronize()
+
+    # Build the system Jacobians
+    jacobians_dense.build(model=model, data=data, limits=limits.data, contacts=contacts.data)
+    jacobians_sparse.build(model=model, data=data, limits=limits.data, contacts=contacts.data)
+    wp.synchronize()
+
+    # Create arrays for the constraint multipliers and initialize them
+    lambdas_start, lambdas = make_constraint_multiplier_arrays(model)
+    lambdas.fill_(1.0)
+
+    # Initialize the generalized joint actuation forces
+    data.joints.tau_j.fill_(1.0)
+
+    # Compute the wrenches using the dense Jacobians
+    compute_joint_dof_body_wrenches_dense(
+        model=model,
+        data=data,
+        jacobians=jacobians_dense,
+        reset_to_zero=True,
+    )
+    compute_constraint_body_wrenches_dense(
+        model=model,
+        data=data,
+        jacobians=jacobians_dense,
+        lambdas_offsets=lambdas_start,
+        lambdas_data=lambdas,
+        limits=limits.data,
+        contacts=contacts.data,
+        reset_to_zero=True,
+    )
+    w_a_i_dense_np = data.bodies.w_a_i.numpy().copy()
+    w_j_i_dense_np = data.bodies.w_j_i.numpy().copy()
+    w_l_i_dense_np = data.bodies.w_l_i.numpy().copy()
+    w_c_i_dense_np = data.bodies.w_c_i.numpy().copy()
+
+    # Compute the wrenches using the sparse Jacobians
+    compute_joint_dof_body_wrenches_sparse(
+        model=model,
+        data=data,
+        jacobians=jacobians_sparse,
+        reset_to_zero=True,
+    )
+    compute_constraint_body_wrenches_sparse(
+        model=model,
+        data=data,
+        jacobians=jacobians_sparse,
+        lambdas_offsets=lambdas_start,
+        lambdas_data=lambdas,
+        reset_to_zero=True,
+    )
+    w_a_i_sparse_np = data.bodies.w_a_i.numpy().copy()
+    w_j_i_sparse_np = data.bodies.w_j_i.numpy().copy()
+    w_l_i_sparse_np = data.bodies.w_l_i.numpy().copy()
+    w_c_i_sparse_np = data.bodies.w_c_i.numpy().copy()
+
+    # Check that the wrenches computed using the dense and sparse Jacobians are close
+    np.testing.assert_allclose(w_a_i_dense_np, w_a_i_sparse_np, rtol=rtol, atol=atol)
+    np.testing.assert_allclose(w_j_i_dense_np, w_j_i_sparse_np, rtol=rtol, atol=atol)
+    np.testing.assert_allclose(w_l_i_dense_np, w_l_i_sparse_np, rtol=rtol, atol=atol)
+    np.testing.assert_allclose(w_c_i_dense_np, w_c_i_sparse_np, rtol=rtol, atol=atol)
 
 
 ###
@@ -85,70 +170,15 @@ class TestDynamicsWrenches(unittest.TestCase):
             verbose=self.verbose,
         )
 
-        # Create the Jacobians container
-        jacobians_dense = DenseSystemJacobians(model=model, limits=limits, contacts=contacts)
-        jacobians_sparse = SparseSystemJacobians(model=model, limits=limits, contacts=contacts)
-        wp.synchronize()
-
-        # Build the system Jacobians
-        jacobians_dense.build(model=model, data=data, limits=limits.data, contacts=contacts.data)
-        jacobians_sparse.build(model=model, data=data, limits=limits.data, contacts=contacts.data)
-        wp.synchronize()
-
-        # Create arrays for the constraint multipliers and initialize them
-        lambdas_start, lambdas = make_constraint_multiplier_arrays(model)
-        lambdas.fill_(1.0)
-
-        # Initialize the generalized joint actuation forces
-        data.joints.tau_j.fill_(1.0)
-
-        # Compute the wrenches using the dense Jacobians
-        compute_joint_dof_body_wrenches_dense(
+        # Compute and compare the wrenches using the dense and sparse Jacobians
+        compute_and_compare_dense_sparse_jacobian_wrenches(
             model=model,
             data=data,
-            jacobians=jacobians_dense,
-            reset_to_zero=True,
+            limits=limits,
+            contacts=contacts,
+            rtol=test_wrench_rtol,
+            atol=test_wrench_atol,
         )
-        compute_constraint_body_wrenches_dense(
-            model=model,
-            data=data,
-            jacobians=jacobians_dense,
-            lambdas_offsets=lambdas_start,
-            lambdas_data=lambdas,
-            limits=limits.data,
-            contacts=contacts.data,
-            reset_to_zero=True,
-        )
-        w_a_i_dense_np = data.bodies.w_a_i.numpy().copy()
-        w_j_i_dense_np = data.bodies.w_j_i.numpy().copy()
-        w_l_i_dense_np = data.bodies.w_l_i.numpy().copy()
-        w_c_i_dense_np = data.bodies.w_c_i.numpy().copy()
-
-        # Compute the wrenches using the sparse Jacobians
-        compute_joint_dof_body_wrenches_sparse(
-            model=model,
-            data=data,
-            jacobians=jacobians_sparse,
-            reset_to_zero=True,
-        )
-        compute_constraint_body_wrenches_sparse(
-            model=model,
-            data=data,
-            jacobians=jacobians_sparse,
-            lambdas_offsets=lambdas_start,
-            lambdas_data=lambdas,
-            reset_to_zero=True,
-        )
-        w_a_i_sparse_np = data.bodies.w_a_i.numpy().copy()
-        w_j_i_sparse_np = data.bodies.w_j_i.numpy().copy()
-        w_l_i_sparse_np = data.bodies.w_l_i.numpy().copy()
-        w_c_i_sparse_np = data.bodies.w_c_i.numpy().copy()
-
-        # Check that the wrenches computed using the dense and sparse Jacobians are close
-        np.testing.assert_allclose(w_a_i_dense_np, w_a_i_sparse_np, rtol=1e-5, atol=1e-8)
-        np.testing.assert_allclose(w_j_i_dense_np, w_j_i_sparse_np, rtol=1e-5, atol=1e-8)
-        np.testing.assert_allclose(w_l_i_dense_np, w_l_i_sparse_np, rtol=1e-5, atol=1e-8)
-        np.testing.assert_allclose(w_c_i_dense_np, w_c_i_sparse_np, rtol=1e-5, atol=1e-8)
 
     def test_02_compute_wrenches_for_multiple_fourbars_with_limits_and_contacts(self):
         # Construct the test problem
@@ -161,70 +191,15 @@ class TestDynamicsWrenches(unittest.TestCase):
             verbose=self.verbose,
         )
 
-        # Create the Jacobians container
-        jacobians_dense = DenseSystemJacobians(model=model, limits=limits, contacts=contacts)
-        jacobians_sparse = SparseSystemJacobians(model=model, limits=limits, contacts=contacts)
-        wp.synchronize()
-
-        # Build the system Jacobians
-        jacobians_dense.build(model=model, data=data, limits=limits.data, contacts=contacts.data)
-        jacobians_sparse.build(model=model, data=data, limits=limits.data, contacts=contacts.data)
-        wp.synchronize()
-
-        # Create arrays for the constraint multipliers and initialize them
-        lambdas_start, lambdas = make_constraint_multiplier_arrays(model)
-        lambdas.fill_(1.0)
-
-        # Initialize the generalized joint actuation forces
-        data.joints.tau_j.fill_(1.0)
-
-        # Compute the wrenches using the dense Jacobians
-        compute_joint_dof_body_wrenches_dense(
+        # Compute and compare the wrenches using the dense and sparse Jacobians
+        compute_and_compare_dense_sparse_jacobian_wrenches(
             model=model,
             data=data,
-            jacobians=jacobians_dense,
-            reset_to_zero=True,
+            limits=limits,
+            contacts=contacts,
+            rtol=test_wrench_rtol,
+            atol=test_wrench_atol,
         )
-        compute_constraint_body_wrenches_dense(
-            model=model,
-            data=data,
-            jacobians=jacobians_dense,
-            lambdas_offsets=lambdas_start,
-            lambdas_data=lambdas,
-            limits=limits.data,
-            contacts=contacts.data,
-            reset_to_zero=True,
-        )
-        w_a_i_dense_np = data.bodies.w_a_i.numpy().copy()
-        w_j_i_dense_np = data.bodies.w_j_i.numpy().copy()
-        w_l_i_dense_np = data.bodies.w_l_i.numpy().copy()
-        w_c_i_dense_np = data.bodies.w_c_i.numpy().copy()
-
-        # Compute the wrenches using the sparse Jacobians
-        compute_joint_dof_body_wrenches_sparse(
-            model=model,
-            data=data,
-            jacobians=jacobians_sparse,
-            reset_to_zero=True,
-        )
-        compute_constraint_body_wrenches_sparse(
-            model=model,
-            data=data,
-            jacobians=jacobians_sparse,
-            lambdas_offsets=lambdas_start,
-            lambdas_data=lambdas,
-            reset_to_zero=True,
-        )
-        w_a_i_sparse_np = data.bodies.w_a_i.numpy().copy()
-        w_j_i_sparse_np = data.bodies.w_j_i.numpy().copy()
-        w_l_i_sparse_np = data.bodies.w_l_i.numpy().copy()
-        w_c_i_sparse_np = data.bodies.w_c_i.numpy().copy()
-
-        # Check that the wrenches computed using the dense and sparse Jacobians are close
-        np.testing.assert_allclose(w_a_i_dense_np, w_a_i_sparse_np, rtol=1e-5, atol=1e-8)
-        np.testing.assert_allclose(w_j_i_dense_np, w_j_i_sparse_np, rtol=1e-5, atol=1e-8)
-        np.testing.assert_allclose(w_l_i_dense_np, w_l_i_sparse_np, rtol=1e-5, atol=1e-8)
-        np.testing.assert_allclose(w_c_i_dense_np, w_c_i_sparse_np, rtol=1e-5, atol=1e-8)
 
     def test_03_compute_wrenches_heterogeneous_model_with_limits_and_contacts(self):
         pass  # TODO
--- newton/_src/solvers/kamino/tests/utils/make.py
+++ newton/_src/solvers/kamino/tests/utils/make.py
@@ -14,52 +14,57 @@
 # limitations under the License.
 
 """
-KAMINO: UNIT TESTS: GENERAL UTILITIES
+Utilities for constructing test problem containers and data.
 """
+
+import math
+from collections.abc import Callable
 
 import numpy as np
 import warp as wp
-from warp.context import Devicelike
 
-from newton._src.solvers.kamino.core.bodies import update_body_inertias
-from newton._src.solvers.kamino.core.builder import ModelBuilder
-from newton._src.solvers.kamino.core.model import Model, ModelData
-from newton._src.solvers.kamino.geometry.detector import CollisionDetector, CollisionDetectorSettings
-from newton._src.solvers.kamino.kinematics.constraints import make_unilateral_constraints_info, update_constraints_info
-from newton._src.solvers.kamino.kinematics.jacobians import DenseSystemJacobians, SparseSystemJacobians
-from newton._src.solvers.kamino.kinematics.joints import compute_joints_data
-from newton._src.solvers.kamino.kinematics.limits import Limits
-from newton._src.solvers.kamino.utils.sim import Simulator
+from ...core.bodies import update_body_inertias
+from ...core.builder import ModelBuilder
+from ...core.math import quat_exp, screw, screw_angular, screw_linear
+from ...core.model import Model, ModelData
+from ...core.types import float32, int32, mat33f, transformf, vec3f, vec6f
+from ...geometry.contacts import Contacts
+from ...geometry.detector import CollisionDetector, CollisionDetectorSettings
+from ...kinematics.constraints import make_unilateral_constraints_info, update_constraints_info
+from ...kinematics.jacobians import DenseSystemJacobians, SparseSystemJacobians
+from ...kinematics.joints import compute_joints_data
+from ...kinematics.limits import Limits
+from ...models.builders import basics as _model_basics
+from ...models.builders import utils as _model_utils
+from .print import (
+    print_model_constraint_info,
+    print_model_data_info,
+)
 
 ###
-# Helper functions
+# Module interface
 ###
 
+__all__ = [
+    "make_containers",
+    "make_generalized_mass_matrices",
+    "make_inverse_generalized_mass_matrices",
+    "make_test_problem",
+    "make_test_problem_fourbar",
+    "update_containers",
+]
 
-def make_simulator(nw: int, model_build_func, device=None) -> Simulator:
-    # Create a model builder
-    builder = ModelBuilder()
 
-    # Construct a specific test model in the builder
-    _, _, _ = model_build_func(builder)
+###
+# Module configs
+###
 
-    # Create additional builders and add them to the main builder
-    for _ in range(nw - 1):
-        other = ModelBuilder()
-        _, _, _ = model_build_func(other)
-        builder.add_builder(other)
+wp.set_module_options({"enable_backward": False})
 
-    sim = Simulator(builder=builder, device=device)
 
-    # Run only the parts of the simulation pipeline that are needed for the test
-    sim._collide()
-    sim._forward_intermediate()
-    sim._forward_kinematics()
-
-    # Create the ready-to-use Simulator instance
-    # NOTE: We have already called the necessary forward
-    # methods to update the internal data containers
-    return sim
+###
+# NumPy Reference Data Generators
+###
 
 
 def make_generalized_mass_matrices(model: Model, data: ModelData) -> list[np.ndarray]:
@@ -110,12 +115,17 @@ def make_inverse_generalized_mass_matrices(model: Model, data: ModelData) -> lis
     return invM_np
 
 
+###
+# Test Problem Scaffolding
+###
+
+
 def make_containers(
     builder: ModelBuilder,
+    device: wp.DeviceLike = None,
     max_world_contacts: int = 0,
-    dt: float = 0.001,
-    device: Devicelike = None,
     sparse: bool = True,
+    dt: float = 0.001,
 ) -> tuple[Model, ModelData, Limits, CollisionDetector, DenseSystemJacobians | SparseSystemJacobians]:
     # Create the model from the builder
     model = builder.finalize(device=device)
@@ -177,3 +187,224 @@ def update_containers(
     # Build the dense system Jacobians
     jacobians.build(model=model, data=data, limits=limits.data, contacts=detector.contacts.data)
     wp.synchronize()
+
+
+def make_test_problem(
+    builder: ModelBuilder,
+    set_state_fn: Callable[[Model, ModelData], None] | None = None,
+    device: wp.DeviceLike = None,
+    max_world_contacts: int = 12,
+    with_limits: bool = False,
+    with_contacts: bool = False,
+    verbose: bool = False,
+) -> tuple[Model, ModelData, Limits | None, Contacts | None]:
+    # Create the model from the builder
+    model = builder.finalize(device=device)
+
+    # Create a model state container
+    data = model.data(device=device)
+
+    # Construct and allocate the limits container
+    limits = None
+    if with_limits:
+        limits = Limits(model=model, device=device)
+
+    # Create the collision detector
+    contacts = None
+    if with_contacts:
+        settings = CollisionDetectorSettings(max_contacts_per_world=max_world_contacts, pipeline="primitive")
+        detector = CollisionDetector(model=model, builder=builder, settings=settings, device=device)
+        contacts = detector.contacts
+
+    # Create the constraints info
+    make_unilateral_constraints_info(
+        model=model,
+        data=data,
+        limits=limits,
+        contacts=contacts,
+        device=device,
+    )
+    if verbose:
+        print("")  # Add a newline for better readability
+        print_model_constraint_info(model)
+        print_model_data_info(data)
+        print("\n")  # Add a newline for better readability
+
+    # If a set-state callback is provided, perturb the system
+    # NOTE: This is done to potentially trigger
+    # joint limits and contacts to become active
+    if set_state_fn is not None:
+        set_state_fn(model=model, data=data)
+        wp.synchronize()
+    if verbose:
+        print("data.bodies.q_i:\n", data.bodies.q_i)
+        print("data.bodies.u_i:\n\n", data.bodies.u_i)
+
+    # Compute the joints state
+    compute_joints_data(model=model, data=data, q_j_p=wp.zeros_like(data.joints.q_j))
+    wp.synchronize()
+    if verbose:
+        print("data.joints.p_j:\n", data.joints.p_j)
+        print("data.joints.r_j:\n", data.joints.r_j)
+        print("data.joints.dr_j:\n", data.joints.dr_j)
+        print("data.joints.q_j:\n", data.joints.q_j)
+        print("data.joints.dq_j:\n\n", data.joints.dq_j)
+
+    # Run limit detection to generate active limits
+    if with_limits:
+        limits.detect(model, data)
+        wp.synchronize()
+        if verbose:
+            print(f"limits.world_active_limits: {limits.world_active_limits}")
+            print(f"data.info.num_limits: {data.info.num_limits}\n\n")
+
+    # Run collision detection to generate active contacts
+    if with_contacts:
+        detector.collide(model, data)
+        wp.synchronize()
+        if verbose:
+            print(f"contacts.world_active_contacts: {detector.contacts.world_active_contacts}")
+            print(f"data.info.num_contacts: {data.info.num_contacts}\n\n")
+
+    # Update the constraints info
+    update_constraints_info(model=model, data=data)
+    if verbose:
+        print("")  # Add a newline for better readability
+        print_model_data_info(data)
+        print("\n")  # Add a newline for better readability
+    wp.synchronize()
+
+    # Return the problem data containers
+    return model, data, limits, contacts
+
+
+###
+# Fourbar
+#
+# Generates a problem using basics.boxes_fourbar model with a specific
+# state configuration that induces active joint limits and contacts.
+###
+
+Q_X_J = 0.3 * math.pi
+THETA_Y_J = 0.0
+THETA_Z_J = 0.0
+J_DR_J = vec3f(0.0)
+J_DV_J = vec3f(0.0)
+J_DOMEGA_J = vec3f(0.0)
+
+
+@wp.kernel
+def _set_fourbar_body_states(
+    model_joint_bid_B: wp.array(dtype=int32),
+    model_joint_bid_F: wp.array(dtype=int32),
+    model_joint_B_r_Bj: wp.array(dtype=vec3f),
+    model_joint_F_r_Fj: wp.array(dtype=vec3f),
+    model_joint_X_j: wp.array(dtype=mat33f),
+    state_body_q_i: wp.array(dtype=transformf),
+    state_body_u_i: wp.array(dtype=vec6f),
+):
+    """
+    Set the state of the bodies to a certain values in order to check computations of joint states.
+    """
+    # Retrieve the thread index as the joint index
+    jid = wp.tid()
+
+    # Retrieve the joint parameters
+    bid_B = model_joint_bid_B[jid]
+    bid_F = model_joint_bid_F[jid]
+    B_r_Bj = model_joint_B_r_Bj[jid]
+    F_r_Fj = model_joint_F_r_Fj[jid]
+    X_j = model_joint_X_j[jid]
+
+    # Retrieve the current state of the Base body
+    p_B = state_body_q_i[bid_B]
+    u_B = state_body_u_i[bid_B]
+
+    # Extract the position and orientation of the Base body
+    r_B = wp.transform_get_translation(p_B)
+    q_B = wp.transform_get_rotation(p_B)
+    R_B = wp.quat_to_matrix(q_B)
+
+    # Extract the linear and angular velocity of the Base body
+    v_B = screw_linear(u_B)
+    omega_B = screw_angular(u_B)
+
+    # Define the joint rotation offset
+    # NOTE: X_j projects quantities into the joint frame
+    # NOTE: X_j^T projects quantities into the outer frame (world or body)
+    q_x_j = Q_X_J * wp.pow(-1.0, float(jid))  # Alternate sign for each joint
+    theta_y_j = THETA_Y_J
+    theta_z_j = THETA_Z_J
+    j_dR_j = vec3f(q_x_j, theta_y_j, theta_z_j)  # Joint offset as rotation vector
+    q_jq = quat_exp(j_dR_j)  # Joint offset as rotation quaternion
+    R_jq = wp.quat_to_matrix(q_jq)  # Joint offset as rotation matrix
+
+    # Define the joint translation offset
+    j_dr_j = J_DR_J
+
+    # Define the joint twist offset
+    j_dv_j = J_DV_J
+    j_domega_j = J_DOMEGA_J
+
+    # Follower body rotation via the Base and joint frames
+    R_B_X_j = R_B @ X_j
+    R_F_new = R_B_X_j @ R_jq @ wp.transpose(X_j)
+    q_F_new = wp.quat_from_matrix(R_F_new)
+
+    # Follower body position via the Base and joint frames
+    r_Fj = R_F_new @ F_r_Fj
+    r_F_new = r_B + R_B @ B_r_Bj + R_B_X_j @ j_dr_j - r_Fj
+
+    # Follower body twist via the Base and joint frames
+    r_Bj = R_B @ B_r_Bj
+    r_Fj = R_F_new @ F_r_Fj
+    omega_F_new = R_B_X_j @ j_domega_j + omega_B
+    v_F_new = R_B_X_j @ j_dv_j + v_B + wp.cross(omega_B, r_Bj) - wp.cross(omega_F_new, r_Fj)
+
+    # Offset the pose of the body by a fixed amount
+    state_body_q_i[bid_F] = wp.transformation(r_F_new, q_F_new, dtype=float32)
+    state_body_u_i[bid_F] = screw(v_F_new, omega_F_new)
+
+
+def set_fourbar_body_states(model: Model, data: ModelData):
+    wp.launch(
+        _set_fourbar_body_states,
+        dim=3,  # Set to three because we only need to set the first three joints
+        inputs=[
+            model.joints.bid_B,
+            model.joints.bid_F,
+            model.joints.B_r_Bj,
+            model.joints.F_r_Fj,
+            model.joints.X_j,
+            data.bodies.q_i,
+            data.bodies.u_i,
+        ],
+    )
+
+
+def make_test_problem_fourbar(
+    device: wp.DeviceLike = None,
+    max_world_contacts: int = 12,
+    num_worlds: int = 1,
+    with_limits: bool = False,
+    with_contacts: bool = False,
+    verbose: bool = False,
+) -> tuple[Model, ModelData, Limits | None, Contacts | None]:
+    # Define the problem using the ModelBuilder
+    builder: ModelBuilder = _model_utils.make_homogeneous_builder(
+        num_worlds=num_worlds,
+        build_fn=_model_basics.build_boxes_fourbar,
+        dynamic_joints=True,
+        implicit_pd=True,
+    )
+
+    # Generate the problem containers using the builder
+    return make_test_problem(
+        builder=builder,
+        set_state_fn=set_fourbar_body_states,
+        device=device,
+        max_world_contacts=max_world_contacts,
+        with_limits=with_limits,
+        with_contacts=with_contacts,
+        verbose=verbose,
+    )

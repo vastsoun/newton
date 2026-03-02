82e6931b8b53a8dc1f9919e335e6b1597c42da4d
Vassilios Tsounis <vassilios.tsounis@disney.com>
2026-03-02 02:57:14 +0100

    Add newton integration examples

---
+++ newton/_src/solvers/kamino/examples/newton/example_dr_legs.py
@@ -0,0 +1,192 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 The Newton Developers
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+###########################################################################
+# Example Robot DR Legs
+#
+# Shows how to simulate DR Legs with multiple worlds using SolverKamino.
+#
+# Command: python -m newton.examples robot_dr_legs --num-worlds 16
+#
+###########################################################################
+
+import os
+
+import warp as wp
+
+import newton
+import newton.examples
+from newton._src.solvers.kamino.models import get_examples_usd_assets_path
+from newton._src.solvers.kamino.utils import logger as msg
+
+
+class Example:
+    def __init__(self, viewer, num_worlds=8, args=None):
+        # TODO
+        self.fps = 50
+        self.frame_dt = 1.0 / self.fps
+        self.sim_time = 0.0
+        self.sim_substeps = 4
+        self.sim_dt = self.frame_dt / self.sim_substeps
+        self.num_worlds = num_worlds
+        self.viewer = viewer
+        self.device = wp.get_device()
+
+        # Create a single-robot model builder and register the Kamino-specific custom attributes
+        robot_builder = newton.ModelBuilder(up_axis=newton.Axis.Z)
+        newton.solvers.SolverKamino.register_custom_attributes(robot_builder)
+        robot_builder.default_shape_cfg.margin = 0.0
+        robot_builder.default_shape_cfg.gap = 0.0
+
+        # Load the DR Legs USD and add it to the builder
+        asset_file = os.path.join(get_examples_usd_assets_path(), "dr_legs/usd/dr_legs_with_meshes_and_boxes.usda")
+        robot_builder.add_usd(
+            asset_file,
+            joint_ordering=None,
+            force_show_colliders=True,
+            force_position_velocity_actuation=True,
+            collapse_fixed_joints=False,  # TODO: FIX THIS WHEN ITS TRUE
+            enable_self_collisions=False,
+            hide_collision_shapes=True,
+        )
+
+        robot_builder.shape_collision_filter_pairs.append((0, 3))
+        msg.debug("robot_builder.shape_collision_filter_pairs: %s", robot_builder.shape_collision_filter_pairs)
+
+        # Add a ground plane
+        # TODO: @nvtw: Remove this once global ground planes are supported
+        robot_builder.add_shape_box(
+            label="ground",
+            body=-1,
+            hx=1.0,
+            hy=1.0,
+            hz=0.1,
+            xform=wp.transformf(0.0, 0.0, -0.2, 0.0, 0.0, 0.0, 1.0),
+            cfg=newton.ModelBuilder.ShapeConfig(margin=0.0, gap=0.0),
+        )
+
+        # Create the multi-world model by duplicating the single-robot
+        # builder for the specified number of worlds
+        builder = newton.ModelBuilder(up_axis=newton.Axis.Z)
+        for _ in range(self.num_worlds):
+            builder.add_world(robot_builder)
+        # TODO: @nvtw: Add support for global ground plane
+        # TODO: builder.add_ground_plane()
+        # builder.add_ground_plane()
+
+        # Create the model from the builder
+        self.model = builder.finalize(
+            # skip_all_validations=True,
+            skip_validation_joints=True,
+            # skip_validation_joint_ordering=True,
+        )
+
+        msg.debug("model.shape_body: %s", self.model.shape_body)
+        msg.debug("model.shape_collision_filter_pairs: %s", self.model.shape_collision_filter_pairs)
+        msg.debug("model.shape_contact_pair_count: %s", self.model.shape_contact_pair_count)
+        msg.debug("model.shape_contact_pairs:\n%s", self.model.shape_contact_pairs)
+
+        # Create the Kamino solver for the given model
+        # TODO: Set solver configurations
+        self.solver = newton.solvers.SolverKamino(self.model)
+
+        # Create state and control data containers
+        self.state_0 = self.model.state()
+        self.state_1 = self.model.state()
+        self.control = self.model.control()
+
+        # Reset the simulation state to a valid initial configuration above the ground
+        self.base_q = wp.zeros(shape=(self.num_worlds,), dtype=wp.transformf)
+        q_b = wp.quat_identity(dtype=wp.float32)
+        q_base = wp.transformf((0.0, 0.0, 0.3), q_b)
+        self.base_q.assign([q_base] * self.num_worlds)
+        self.solver.reset(state_out=self.state_0, base_q=self.base_q)
+
+        # Attach the model to the viewer for visualization
+        self.viewer.set_model(self.model)
+
+        # Capture the simulation graph if running on CUDA
+        # NOTE: This only has an effect on GPU devices
+        self.capture()
+
+    def capture(self):
+        self.graph = None
+        if self.device.is_cuda:
+            with wp.ScopedCapture() as capture:
+                self.simulate()
+            self.graph = capture.graph
+
+    # simulate() performs one frame's worth of updates
+    def simulate(self):
+        for _ in range(self.sim_substeps):
+            # clear forces on the state before applying new ones
+            self.state_0.clear_forces()
+            # apply forces to the model for picking, wind, etc
+            self.viewer.apply_forces(self.state_0)
+            # step the simulation forward by one time step
+            self.solver.step(self.state_0, self.state_1, self.control, None, self.sim_dt)
+            # swap states
+            self.state_0, self.state_1 = self.state_1, self.state_0
+
+    def step(self):
+        if self.graph:
+            wp.capture_launch(self.graph)
+        else:
+            self.simulate()
+
+        self.sim_time += self.frame_dt
+
+    def render(self):
+        self.viewer.begin_frame(self.sim_time)
+        self.viewer.log_state(self.state_0)
+        # TODO: self.viewer.log_contacts(self.contacts, self.state_0)
+        self.viewer.end_frame()
+
+    def test_final(self):
+        newton.examples.test_body_state(
+            self.model,
+            self.state_0,
+            "all bodies are above the ground",
+            lambda q, qd: q[2] > -0.006,
+        )
+        # Only check velocities on CUDA where we run 500 frames (enough time to settle)
+        # On CPU we only run 10 frames and the robot is still falling (~0.65 m/s)
+        if self.device.is_cuda:
+            newton.examples.test_body_state(
+                self.model,
+                self.state_0,
+                "body velocities are small",
+                lambda q, qd: max(abs(qd))
+                < 0.25,  # Relaxed from 0.1 - unified pipeline has residual velocities up to ~0.2
+            )
+
+
+if __name__ == "__main__":
+    parser = newton.examples.create_parser()
+    parser.add_argument("--num-worlds", type=int, default=1, help="Total number of simulated worlds.")
+    viewer, args = newton.examples.init(parser)
+    example = Example(viewer, args.num_worlds, args)
+    example.viewer._paused = True  # Start paused to inspect the initial configuration
+
+    # If only a single-world is created, set initial
+    # camera position for better view of the system
+    if args.num_worlds == 1 and hasattr(example.viewer, "set_camera"):
+        camera_pos = wp.vec3(1.34, 0.0, 0.25)
+        pitch = -7.0
+        yaw = -180.0
+        example.viewer.set_camera(camera_pos, pitch, yaw)
+
+    msg.notif("Starting the simulation...")
+    newton.examples.run(example, args)
---
+++ newton/_src/solvers/kamino/examples/newton/example_fourbar.py
@@ -0,0 +1,206 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 The Newton Developers
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+###########################################################################
+# Example for basic four-bar mechanism
+#
+# Shows how to simulate a basic four-bar linkage with multiple worlds using SolverKamino.
+#
+# Command: python -m newton.examples example_fourbar --num-worlds 16
+#
+###########################################################################
+
+# Python imports
+import os
+
+# Numpy/Warp imports
+import numpy as np
+import warp as wp
+
+# Newton imports
+import newton
+import newton.examples
+
+# Kamino imports
+from newton._src.solvers.kamino.geometry import CollisionDetectorSettings
+from newton._src.solvers.kamino.models import get_basics_usd_assets_path
+from newton._src.solvers.kamino.solver_kamino import SolverKaminoSettings
+from newton._src.solvers.kamino.solvers.padmm import PADMMWarmStartMode
+from newton._src.solvers.kamino.solvers.warmstart import WarmstarterContacts
+from newton._src.solvers.kamino.utils import logger as msg
+
+
+class Example:
+    def __init__(self, viewer, num_worlds=8, args=None):
+        # Set simulation run-time configurations
+        self.fps = 60
+        self.frame_dt = 1.0 / self.fps
+        self.sim_time = 0.0
+        self.sim_substeps = 16
+        self.sim_dt = self.frame_dt / self.sim_substeps
+        self.num_worlds = num_worlds
+        self.viewer = viewer
+        self.device = wp.get_device()
+
+        # Create a single-robot model builder and register the Kamino-specific custom attributes
+        msg.notif("Creating and configuring the model builder for Kamino...")
+        robot_builder = newton.ModelBuilder(up_axis=newton.Axis.Z)
+        newton.solvers.SolverKamino.register_custom_attributes(robot_builder)
+        robot_builder.default_shape_cfg.margin = 0.0
+        robot_builder.default_shape_cfg.gap = 0.0
+
+        # Load the Anymal D USD and add it to the builder
+        msg.notif("Loading USD asset and adding it to the model builder...")
+        asset_file = os.path.join(get_basics_usd_assets_path(), "boxes_fourbar.usda")
+        robot_builder.add_usd(
+            asset_file,
+            joint_ordering=None,
+            force_show_colliders=True,
+            force_position_velocity_actuation=True,
+            # bodies_follow_joint_ordering=True, TODO: CHECK THAT THIS WORKS
+            collapse_fixed_joints=False,  # TODO: FIX THIS WHEN ITS TRUE
+            enable_self_collisions=False,
+            hide_collision_shapes=False,
+        )
+
+        # Create the multi-world model by duplicating the single-robot
+        # builder for the specified number of worlds
+        msg.notif(f"Duplicating the model builder for {self.num_worlds} worlds and finalizing the model...")
+        builder = newton.ModelBuilder(up_axis=newton.Axis.Z)
+        for _ in range(self.num_worlds):
+            builder.add_world(robot_builder)
+        # TODO: @nvtw: Add support for global ground plane
+        # TODO: builder.add_ground_plane()
+        # builder.add_ground_plane()
+
+        # Create the model from the builder
+        msg.notif("Creating the model from the builder...")
+        self.model = builder.finalize(skip_validation_joints=True)
+
+        # Create and configure settings for SolverKamino and the collision detector
+        collision_detector_settings = CollisionDetectorSettings()
+        collision_detector_settings.pipeline = "unified"
+        collision_detector_settings.max_contacts = 32 * self.num_worlds
+        solver_settings = SolverKaminoSettings()
+        solver_settings.problem.preconditioning = True
+        solver_settings.padmm.primal_tolerance = 1e-4
+        solver_settings.padmm.dual_tolerance = 1e-4
+        solver_settings.padmm.compl_tolerance = 1e-4
+        solver_settings.padmm.max_iterations = 200
+        solver_settings.padmm.rho_0 = 0.1
+        solver_settings.use_solver_acceleration = True
+        solver_settings.warmstart_mode = PADMMWarmStartMode.CONTAINERS
+        solver_settings.contact_warmstart_method = WarmstarterContacts.Method.GEOM_PAIR_NET_FORCE
+
+        # Create the Kamino solver for the given model
+        msg.notif("Creating the Kamino solver for the given model...")
+        self.solver = newton.solvers.SolverKamino(
+            model=self.model,
+            solver_settings=solver_settings,
+            collision_detector_settings=collision_detector_settings,
+        )
+
+        # Create state and control data containers
+        self.state_0 = self.model.state()
+        self.state_1 = self.model.state()
+        self.control = self.model.control()
+
+        # Reset the simulation state to a valid initial configuration above the ground
+        msg.notif("Resetting the simulation state to a valid initial configuration above the ground...")
+        self.base_q = wp.zeros(shape=(self.num_worlds,), dtype=wp.transformf)
+        q_b = wp.quat_identity(dtype=wp.float32)
+        q_base = wp.transformf((0.0, 0.0, 0.1), q_b)
+        q_base = np.array(q_base)
+        q_base = np.tile(q_base, (self.num_worlds, 1))
+        for w in range(self.num_worlds):
+            q_base[w, :3] += np.array([0.0, 0.0, 0.2]) * float(w)
+        self.base_q.assign(q_base)
+        self.solver.reset(state_out=self.state_0, base_q=self.base_q)
+
+        # Attach the model to the viewer for visualization
+        self.viewer.set_model(self.model)
+
+        # Capture the simulation graph if running on CUDA
+        # NOTE: This only has an effect on GPU devices
+        self.capture()
+
+    def capture(self):
+        self.graph = None
+        if self.device.is_cuda:
+            with wp.ScopedCapture() as capture:
+                self.simulate()
+            self.graph = capture.graph
+
+    # simulate() performs one frame's worth of updates
+    def simulate(self):
+        for _ in range(self.sim_substeps):
+            # clear forces on the state before applying new ones
+            self.state_0.clear_forces()
+            # apply forces to the model for picking, wind, etc
+            self.viewer.apply_forces(self.state_0)
+            # step the simulation forward by one time step
+            self.solver.step(self.state_0, self.state_1, self.control, None, self.sim_dt)
+            # swap states
+            self.state_0, self.state_1 = self.state_1, self.state_0
+
+    def step(self):
+        if self.graph:
+            wp.capture_launch(self.graph)
+        else:
+            self.simulate()
+        self.sim_time += self.frame_dt
+
+    def render(self):
+        self.viewer.begin_frame(self.sim_time)
+        self.viewer.log_state(self.state_0)
+        # TODO: self.viewer.log_contacts(self.contacts, self.state_0)
+        self.viewer.end_frame()
+
+    def test_final(self):
+        newton.examples.test_body_state(
+            self.model,
+            self.state_0,
+            "all bodies are above the ground",
+            lambda q, qd: q[2] > -0.006,
+        )
+        # Only check velocities on CUDA where we run 500 frames (enough time to settle)
+        # On CPU we only run 10 frames and the robot is still falling (~0.65 m/s)
+        if self.device.is_cuda:
+            newton.examples.test_body_state(
+                self.model,
+                self.state_0,
+                "body velocities are small",
+                lambda q, qd: max(abs(qd))
+                < 0.25,  # Relaxed from 0.1 - unified pipeline has residual velocities up to ~0.2
+            )
+
+
+if __name__ == "__main__":
+    parser = newton.examples.create_parser()
+    parser.add_argument("--num-worlds", type=int, default=1, help="Total number of simulated worlds.")
+    viewer, args = newton.examples.init(parser)
+    example = Example(viewer, args.num_worlds, args)
+    example.viewer._paused = True  # Start paused to inspect the initial configuration
+
+    # If only a single-world is created, set initial
+    # camera position for better view of the system
+    if args.num_worlds == 1 and hasattr(example.viewer, "set_camera"):
+        camera_pos = wp.vec3(-0.5, -1.0, 0.2)
+        pitch = -5.0
+        yaw = 70.0
+        example.viewer.set_camera(camera_pos, pitch, yaw)
+
+    msg.notif("Starting the simulation...")
+    newton.examples.run(example, args)

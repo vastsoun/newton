92323d995a1e119b6c70ef816995da3ac1770a26
Eric Heiden <eheiden@nvidia.com>
2026-03-10 13:16:02 -0700

    Fix parser creation in sensor tiled camera example (#2041)
    Signed-off-by: Eric Heiden <eric-heiden@outlook.com>

--- newton/examples/sensors/example_sensor_tiled_camera.py
+++ newton/examples/sensors/example_sensor_tiled_camera.py
@@ -561,20 +561,25 @@ class Example:
 
         imgui.end()
 
+    @staticmethod
+    def create_parser():
+        parser = newton.examples.create_parser()
+        parser.add_argument(
+            "--ply",
+            help="Gaussian filename.",
+        )
+        parser.add_argument(
+            "-min",
+            "--min-response",
+            type=float,
+            default=0.1,
+            help="Gaussian min response.",
+        )
+        return parser
+
 
 if __name__ == "__main__":
-    parser = newton.examples.create_parser()
-    parser.add_argument(
-        "--ply",
-        help="Gaussian Filename",
-    )
-    parser.add_argument(
-        "-min",
-        "--min-response",
-        type=float,
-        default=0.1,
-        help="Gaussian min response",
-    )
+    parser = Example.create_parser()
 
     # Parse arguments and initialize viewer
     viewer, args = newton.examples.init(parser)

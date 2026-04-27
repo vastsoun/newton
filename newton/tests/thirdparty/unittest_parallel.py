0d3f1be883537346275786dbeafaa32f1e1c5fd3
adenzler-nvidia <adenzler@nvidia.com>
2026-04-27 13:05:25 +0000

    Bump parallel test executor timeout to 50 min (#2596)

--- newton/tests/thirdparty/unittest_parallel.py
+++ newton/tests/thirdparty/unittest_parallel.py
@@ -243,7 +243,7 @@ def main(argv=None):
                         executor_kwargs["max_tasks_per_child"] = 1
                     with concurrent.futures.ProcessPoolExecutor(**executor_kwargs) as executor:
                         test_manager = ParallelTestManager(manager, args, temp_dir)
-                        results = list(executor.map(test_manager.run_tests, test_suites, timeout=2400))
+                        results = list(executor.map(test_manager.run_tests, test_suites, timeout=3000))
         else:
             # This entire path is an NVIDIA Modification
 

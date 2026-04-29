Fix histogram plotting bin computation

newton/_src/solvers/kamino/_src/utils/sim/datalog.py
@@ -293,7 +293,7 @@ class SimulationLogger:
         num_iters_data = self.log_padmm_iters[: self._frames]
         self.plt.hist(
             num_iters_data - 0.5,  # Center histogram bar at integer values for readability
-            bins=np.max(num_iters_data) - np.min(num_iters_data),  # Ensure there is one bar per integer
+            bins=np.max(num_iters_data) - np.min(num_iters_data) + 1,  # Ensure there is one bar per integer
         )
         self.plt.yscale("log")  # Make Y-axis logarithmic
         self.plt.title("Histogram of PADMM Solver Iterations")
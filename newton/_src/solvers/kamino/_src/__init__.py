04e4c60563087af2eb3864b2f7dbbaa079728fbf
Vassilios Tsounis <vassilios.tsounis@disney.com>
2026-05-05 20:57:58 +0200

    Implement computing `body_parent_f` for Kamino solver if extended attribute is present in outbound state container

--- newton/_src/solvers/kamino/_src/__init__.py
+++ newton/_src/solvers/kamino/_src/__init__.py
@@ -15,6 +15,7 @@ from .core.conversions import convert_model_joint_transforms
 from .core.gravity import convert_model_gravity
 from .core.model import ModelKamino
 from .core.state import StateKamino
+from .dynamics.wrenches import convert_joint_wrenches_to_body_parent_wrenches
 from .geometry.contacts import (
     ContactsKamino,
     convert_contacts_kamino_to_newton,
@@ -40,6 +41,7 @@ __all__ = [
     "convert_body_origin_to_com",
     "convert_contacts_kamino_to_newton",
     "convert_contacts_newton_to_kamino",
+    "convert_joint_wrenches_to_body_parent_wrenches",
     "convert_model_gravity",
     "convert_model_joint_transforms",
     "msg",
